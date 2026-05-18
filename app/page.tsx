bffc4feaa9f5af2913c2fb46d6c7374370ec44fd
Jarren San Jose <sanjose.jarren@gmail.com>
2026-05-18 16:36:07 -0700

    add footer with github repo link

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import ImageUploader from './components/ImageUploader';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4">
-      <div className="max-w-4xl mx-auto">
-        <ImageUploader />
-      </div>
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <main className="flex-1 py-12 px-4">
+        <div className="max-w-4xl mx-auto">
+          <ImageUploader />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
